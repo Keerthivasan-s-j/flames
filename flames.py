559b21d3f9ba7152a547e3ed2fab3d1c2338a58b
Keerthivasan-s-j <149575925+Keerthivasan-s-j@users.noreply.github.com>
2024-01-20 18:52:50 +0530

    Update flames.py

--- flames.py
+++ flames.py
@@ -1,9 +1,31 @@
 import streamlit as st
-import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import time
+
+def store_data(name,partner_name,char_difference,relation):
+    credentials_path = "C:\\Users\\jagan\\Downloads\\flames-411812-8732b98b4892.json"
+    sheet_key = '1RQ1Dtl75W6vvCnxoaWt5Cp9DICMqMruLZpsETCV3MHQ'
+    worksheet_name = 'Sheet1'
+
+    # Authenticate with Google Sheets API
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
+    gc = gspread.authorize(credentials)
+
+    # Open the Google Sheet by key
+    sh = gc.open_by_key(sheet_key)
+
+    # Select the worksheet by name
+    worksheet = sh.worksheet(worksheet_name)
+    worksheet.append_row([name,partner_name,char_difference,relation])
+
+    print("Data inserted successfully.")
+
 
 #required variables
 password=None
+
 
 #Page Title
 st.set_page_config(page_title="Flames",page_icon=":❤️:")
@@ -22,12 +44,9 @@ st.sidebar.markdown("---")
 st.sidebar.title(":green[About Developer]")
 st.sidebar.write(":gray[This web application is developed by :violet[Keerthivasan S J]]")
 
-    
-
 #Contact information
 st.sidebar.title(":green[Reach out]")
 st.sidebar.write(":blue[**:orange[Email]** : keerthivasan.cs22.krct.ac.in]")
-
 
 
 #Flames Logic
@@ -77,7 +96,9 @@ if st.button(":orange[Submit]"):
             message="Don't take it serious it's just a game😇"
         elif result[1]=="SISTER":
             message="You both are good brother and sisters 🧑‍🤝‍🧑"
-            
+
+        store_data(bname,gname,result[0],result[1])
+
         st.header(f":violet[{result[0]}] Character differ from both name")
         st.title(f"Your Relation :red[{result[1]}]")
         st.write(message)
